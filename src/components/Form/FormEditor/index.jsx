6a82a3a2355a90bb3fbcc4c7f4bb1f5d338d499e
Gia Phu <phu.tran@r-digital.tech>
2022-12-08 13:54:26 +0700

    DAM Intergration

--- src/components/Form/FormEditor/index.jsx
+++ src/components/Form/FormEditor/index.jsx
@@ -1,17 +1,40 @@
 import ClassicEditor from 'ckeditor5-custom-build/build/ckeditor';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import React from 'react';
-
+import React, { useState } from 'react';
+import ModalDAMComponent from 'components/ModalDamComponent';
+import { Icon } from '@iconify/react';
+import styles from './index.module.scss';
 const Editor = ({ field }) => {
+  const [editor, setEditor] = useState('');
+  const [show, setShow] = useState(false);
+
+  const onSelect = (data) => {
+    editor.model.change(() => {
+      const imgTag = `<img  src="${data[0]?.url}" alt="${data[0]?.basename}"></img>`;
+      const viewFragment = editor.data.processor.toView(imgTag);
+      const modelFragment = editor.data.toModel(viewFragment);
+      editor.model.insertContent(modelFragment);
+    });
+    setShow(false);
+  };
+
   return (
-    <div key={field.key}>
+    <div key={field.key} className="position-relative">
+      <p
+        onClick={() => setShow(true)}
+        className={`${styles['image-upload-button']} position-absolute bottom-0 end-0 zindex-1 mb-0 cursor-pointer`}
+      >
+        <Icon width={20} height={20} className="text-black" icon="ion:image-outline" />
+      </p>
+      <ModalDAMComponent show={show} onHide={() => setShow(false)} onSelect={onSelect} />
       <CKEditor
         editor={ClassicEditor}
         data={field?.value ?? ''}
         onReady={(editor) => {
+          setEditor(editor);
           editor.editing.view.change((writer) => {
             writer.setStyle(
-              { 'max-height': '400px', 'min-height': '200px' },
+              { 'max-height': '500px', 'min-height': '200px', 'padding-bottom': '20px' },
               editor.editing.view.document.getRoot()
             );
           });
--- src/components/Form/FormImage/index.jsx
+++ src/components/Form/FormImage/index.jsx
@@ -1,67 +1,54 @@
-import { Icon } from "@iconify/react";
-import React, { useState } from "react";
-import { useDropzone } from "react-dropzone";
-import ComponentImage from "../../ComponentImage";
-import styles from "./index.module.scss";
+import { Icon } from '@iconify/react';
+import React, { lazy, useState } from 'react';
+import ComponentImage from '../../ComponentImage';
+import styles from './index.module.scss';
+
+const ModalDAMComponent = lazy(() => import('components/ModalDamComponent'));
+
 const FormImage = ({ field }) => {
+  const [show, setShow] = useState(false);
   const [file, setFile] = useState(field.value);
-
-  const { getRootProps, getInputProps, open } = useDropzone({
-    accept: "image/*",
-    maxFiles: 1,
-    multiple: false,
-    onDrop: (acceptedFiles) => {
-      setFile(URL.createObjectURL(acceptedFiles[0]));
-    },
-    noClick: true,
-  });
-
+  const onSelect = (data) => {
+    setFile(data[0]);
+    setShow(false);
+  };
   return (
-    <div className={`${styles["border"]} position-relative p-sm`}>
-      {file && (
-        <div
-          className={`position-absolute top-0 start-100 text-end cursor-pointer`}
-        >
-          <div className="bg-danger p-sm" onClick={() => setFile(null)}>
-            <Icon
-              className="text-white"
-              width={24}
-              height={24}
-              icon="ion:trash-outline"
-            />
-          </div>
-        </div>
-      )}
-      <div
-        {...getRootProps()}
-        className="d-flex align-items-center justify-content-center cursor-auto"
-      >
-        <input
-          {...getInputProps()}
-          className="position-absolute start-0 top-0 bottom-0 end-0 cursor-auto"
-        />
-        {file ? (
-          <div
-            key={field.value}
-            className={`${styles["limit-image"]} text-center cursor-pointer`}
-            onClick={open}
-          >
-            <ComponentImage src={file} alt={field.value} />
-          </div>
-        ) : (
-          <div className="d-flex flex-column align-items-center p-4">
-            <p>Drop files anywhere to upload</p>
-            <p className="text-secondary">or</p>
-            <p
-              className="btn bg-white text-secondary border rounded-1 py-11 px-3 mb-0"
-              onClick={open}
-            >
-              Select File
-            </p>
+    <>
+      <ModalDAMComponent show={show} onHide={() => setShow(false)} onSelect={onSelect} />
+      <div className={`position-relative p-sm`}>
+        {file && (
+          <div className={`position-absolute top-0 start-100 text-end cursor-pointer`}>
+            <div className="bg-danger p-sm" onClick={() => setFile(null)}>
+              <Icon className="text-white" width={24} height={24} icon="ion:trash-outline" />
+            </div>
           </div>
         )}
+        <div className="d-flex align-items-center justify-content-center cursor-auto">
+          <input
+            readOnly
+            className={`${styles['border']} position-absolute start-0 top-0 bottom-0 end-0 cursor-auto`}
+          />
+          {file ? (
+            <div
+              key={field.value}
+              className={`${styles['limit-image']} text-center cursor-pointer zindex-1`}
+            >
+              <ComponentImage src={file.url} alt={file?.basename ?? 'Image'} />
+            </div>
+          ) : (
+            <div className="d-flex flex-column align-items-center p-4 zindex-1">
+              <p className="mb-3">Select file to upload</p>
+              <p
+                onClick={() => setShow(true)}
+                className="btn bg-white text-secondary border rounded-1 py-11 px-3"
+              >
+                Select File
+              </p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
