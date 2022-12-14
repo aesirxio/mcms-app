43d9d3ebdb1e47f190b0f448ce6839de8148cc79
Vinh.nguyen <vinh.nguyen@redweb.dk>
2022-12-14 17:50:32 +0700

    pipeline

--- src/containers/DmaPage/index.jsx
+++ src/containers/DmaPage/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { AesirXDmaChannels } from 'aesirx-dma-app';
+// import { AesirXDmaChannels } from 'aesirx-dma-app';
 import './index.scss';
 
 const DmaPage = () => {
-  return (
-    <div className="py-4 px-3 h-100 ">
-      <AesirXDmaChannels />
-    </div>
-  );
+  return <div className="py-4 px-3 h-100 ">{/* <AesirXDmaChannels /> */}</div>;
 };
 
 export default DmaPage;
