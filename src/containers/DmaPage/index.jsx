7ac0aaf51be2ab198695c3a1b592a53cf5ce7ede
Vinh.nguyen <vinh.nguyen@redweb.dk>
2022-12-19 11:50:44 +0700

    Waiting update DMA intergrate

--- src/containers/DmaPage/index.jsx
+++ src/containers/DmaPage/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { AesirXDmaChannels } from 'aesirx-dma-app';
+// import { AesirXDmaChannels } from 'aesirx-dma-app';
 import './index.scss';
 
 const DmaPage = () => {
   return (
-    <div className="py-4 px-3 h-100 ">
-      <AesirXDmaChannels />
-    </div>
+    <div className="py-4 px-3 h-100 ">{/* <AesirXDmaChannels /> */} Waiting update lib ...</div>
   );
 };
 
