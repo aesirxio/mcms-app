dc6dead4b70252ef19e25321bd12f4ed6447e18b
Vinh.nguyen <vinh.nguyen@redweb.dk>
2023-01-03 16:06:15 +0700

    Login Demo

--- src/containers/ItemsPage/ItemsViewModels/ItemsListViewModel.js
+++ src/containers/ItemsPage/ItemsViewModels/ItemsListViewModel.js
@@ -105,7 +105,7 @@ class ItemsListViewModel {
 
   callbackOnSuccessHandler = (result) => {
     if (result) {
-      this.tableData = result?.results.items ?? [];
+      this.tableData = result?.results?.items ?? [];
       this.pagination = result?.pagination;
     }
     this.formStatus = PAGE_STATUS.READY;
