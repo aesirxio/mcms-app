a9e13805b1932d6a9a33c78a45d0e6e04241ec72
Gia Phu <phu.tran@r-digital.tech>
2023-01-30 16:18:28 +0700

    update UI & color login page

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import "./index.scss";
-import { withTranslation } from "react-i18next";
+import './index.scss';
+import { withTranslation } from 'react-i18next';
 
 class Checkbox extends React.Component {
   render() {
     let { name, text, checked, onCheckBoxChange, disabled } = this.props;
     const { t } = this.props;
     return (
-      <div className="form-check">
+      <div className="form-check mb-0 d-flex align-items-center">
         <input
           name={name}
           id={`id_${name}`}
-          className="form-check-input cursor-pointer"
+          className={`form-check-input cursor-pointer p-11`}
           type="checkbox"
           checked={checked}
           onChange={onCheckBoxChange}
           disabled={disabled}
         />
         <label
-          className="form-check-label cursor-pointer"
+          className="form-check-label cursor-pointer mt-1 ms-2 text-black"
           htmlFor={`id_${name}`}
         >
           {t(text)}
@@ -29,4 +29,4 @@ class Checkbox extends React.Component {
   }
 }
 
-export default withTranslation("common")(Checkbox);
+export default withTranslation('common')(Checkbox);
